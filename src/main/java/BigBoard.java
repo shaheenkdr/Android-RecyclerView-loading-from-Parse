6e487aaa8c49d7143ea819a01263ad34a8cbb94a
shaheenkdr <shaheenkader@outlook.com>
2015-10-26 18:55:10 -0700

    First commit

---
+++ src/main/java/BigBoard.java
@@ -0,0 +1,109 @@
+package obx.com.futurister;
+
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BigBoard extends ActionBarActivity {
+
+    private List<Person> persons;
+    private RecyclerView rv;
+    RVAdapter adapter;
+    String a,b;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Parse.initialize(this, "VOLtsAtw9f2AxM31nVkGfzUg83LwVIJse6wtjnXz", "X4b9xT5YHtgzdGVV4eSPZk3EKevcik4eRRwswV7D");
+        setContentView(R.layout.activity_big_board);
+        initializeData();
+        adapter = new RVAdapter(persons);
+
+
+        rv=(RecyclerView)findViewById(R.id.rv);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+        rv.setHasFixedSize(true);
+
+
+        initializeAdapter();
+
+    }
+
+    private void initializeData(){
+        persons = new ArrayList<>();
+
+
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Credentials");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> credentialList, ParseException e) {
+                if (e == null) {
+                    for(int i=0;i<credentialList.size();i++)
+                    {
+                        a=credentialList.get(i).getString("Name");
+                        b=credentialList.get(i).getString("SurName");
+                        persons.add(new Person(a,b));
+
+
+
+                        Log.d("OUT", "So the Val::------> " +a +b);
+                        adapter.notifyDataSetChanged();
+
+                    }
+                } else {
+                    Log.d("score", "Error: " + e.getMessage());
+                }
+            }
+        });
+        //persons.add(new Person("Emma Wilson", "23 years old"));
+        //persons.add(new Person("Lavery Maiss", "25 years old"));
+        //persons.add(new Person("Lillie Watts", "35 years old"));
+
+    }
+
+    private void initializeAdapter(){
+
+        rv.setAdapter(adapter);
+    }
+
+    public void backOpt(View view)
+    {
+
+    }
+
+    public void boardHelp(View view)
+    {
+
+    }
+
+
+
+
+
+}
+
+
---
+++ src/main/java/Person.java
@@ -0,0 +1,15 @@
+package obx.com.futurister;
+
+class Person
+{
+    String name;
+    String surname;
+
+
+    Person(String name, String sur)
+    {
+        this.name = name;
+        this.surname = sur;
+
+    }
+}
